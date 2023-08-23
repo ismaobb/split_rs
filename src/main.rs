047ce642e112ecdf8d59ce936309c3da0ecb7c8c
maobb <277581480@qq.com>
2023-08-23 15:31:54 +0800

    add global io::Result

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::env;
 use std::fs::{self, File};
-use std::io::{Read, Write};
+use std::io::{Read, Result, Write};
 use std::path::Path;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-fn main() {
+fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
     if args.len() < 2 {
@@ -14,7 +14,7 @@ fn main() {
     // 设置输入文件名和每个分片的大小
     let input_file = &args[1];
 
-    let mut file = File::open(input_file).expect("Failed to open input file");
+    let mut file = File::open(input_file)?;
     let mut buffer = [0; 1 << 20];
 
     let mut chunk_count = 0;
@@ -25,34 +25,30 @@ fn main() {
 
     let output_folder = format!("{time}");
     if !Path::new(&output_folder).exists() {
-        fs::create_dir(&output_folder).expect("Failed to create output folder");
+        fs::create_dir(&output_folder)?;
     }
 
     let start = SystemTime::now();
 
     loop {
-        let bytes_read = file.read(&mut buffer).expect("Failed to read from file");
+        let bytes_read = file.read(&mut buffer)?;
 
         if bytes_read == 0 {
             break;
         }
 
         let output_file_name = format!("{}/chunk_{}", output_folder, chunk_count);
-        let mut output_file =
-            File::create(&output_file_name).expect("Failed to create output file");
-        output_file
-            .write_all(&buffer[..bytes_read])
-            .expect("Failed to write to output file");
+        let mut output_file = File::create(&output_file_name)?;
+        output_file.write_all(&buffer[..bytes_read])?;
 
         chunk_count += 1;
     }
 
-    let end = SystemTime::now();
-    let diff = end.duration_since(start).expect("diff timestamp error");
-
     println!(
-        "File splitting complete. {} chunks created. take {} ms",
+        "File splitting complete. take {} ms. {} chunks created.",
         chunk_count,
-        diff.as_millis()
+        SystemTime::now().duration_since(start).unwrap().as_millis()
     );
+
+    Ok(())
 }
